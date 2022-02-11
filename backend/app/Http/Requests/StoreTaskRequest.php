9c373c50411b3557cb66b5ef8de1778685cc66e4
tokatu4561 <ryobsk4561@gmail.com>
2022-02-11 06:41:43 -0800

    add validation form request

--- backend/app/Http/Requests/StoreTaskRequest.php
+++ backend/app/Http/Requests/StoreTaskRequest.php
@@ -13,7 +13,7 @@ class StoreTaskRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,14 @@ class StoreTaskRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required'
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル'
         ];
     }
 }
--- backend/app/Http/Requests/UpdateTaskRequest.php
+++ backend/app/Http/Requests/UpdateTaskRequest.php
@@ -13,7 +13,7 @@ class UpdateTaskRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,14 @@ class UpdateTaskRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required'
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'title' => 'タイトル'
         ];
     }
 }
