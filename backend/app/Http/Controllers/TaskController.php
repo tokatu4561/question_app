fc133945448242617110e1b86b29df278f6f6855
tokatu4561 <ryobsk4561@gmail.com>
2022-02-11 06:42:12 -0800

    add Task Controller

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -37,7 +37,13 @@ class TaskController extends Controller
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+        $task = new Task();
+
+        $task->title = $request->title;
+
+        $task->save();
+
+        return $task ? response()->json($task, 201) : response()->json([], 500);
     }
 
     /**
@@ -67,21 +73,27 @@ class TaskController extends Controller
      *
      * @param  \App\Http\Requests\UpdateTaskRequest  $request
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $task->title = $request->title;
+
+        $task->save();
+
+        return $task ? response()->json($task, 200) : response()->json([], 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return $task ? response()->json($task, 200) : response()->json([], 500);
     }
 }
--- backend/tests/Feature/TaskTest.php
+++ backend/tests/Feature/TaskTest.php
@@ -20,4 +20,61 @@ class TaskTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonCount($tasks->count());
     }
+
+    /**
+     *@test
+     */
+    public function １件登録する()
+    {
+        $data = [
+            'title' => 'テスト',
+        ];
+
+        $response = $this->postJson('api/tasks', $data)
+            ->assertStatus(201);
+
+        $this->assertDatabaseHas('tasks', $data);
+    }
+
+    /**
+     *@test
+     */
+    public function タイトルがからであれば登録不可()
+    {
+        $data = [
+            'title' => '',
+        ];
+
+        $response = $this->postJson('api/tasks', $data)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['title' => 'タイトルは、必ず指定してください。']);
+    }
+
+    /**
+     *@test
+     */
+    public function １件更新する()
+    {
+        $task = Task::factory()->create();
+
+        $title = 'タスクテスト';
+
+        $response = $this->patchJson("api/tasks/{$task->id}", ['title' => $title])
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('tasks', ['title' => $title]);
+    }
+
+    /**
+     *@test
+     */
+    public function 削除する()
+    {
+        $task = Task::factory()->create();
+
+        $response = $this->deleteJson("api/tasks/{$task->id}")
+            ->assertStatus(200);
+
+        // $this->assertDatabaseHas('tasks', ['title' => $title]);
+    }
 }
