7a6c254093672729a3a5dbb63ca0d10702a3b05b
tokatu4561 <ryobsk4561@gmail.com>
2022-02-11 06:53:50 -0800

    install react typescript tailwindcss

--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+require("laravel-mix-purgecss");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +12,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.ts("resources/js/app.ts", "public/js")
+    .react()
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
