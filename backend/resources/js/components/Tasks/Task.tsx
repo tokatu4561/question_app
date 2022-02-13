5b52cc0f0facc3666df35877732b96a9c3ca5183
tokatu4561 <ryobsk4561@gmail.com>
2022-02-13 04:46:40 -0800

    add checkBox

--- backend/resources/js/components/Tasks/Task.tsx
+++ backend/resources/js/components/Tasks/Task.tsx
@@ -1,25 +1,24 @@
 import { Link } from "react-router-dom";
 
-type props = {
-    id: string;
-    title: string;
-};
+import { TaskType } from "../../types/task";
 
-export const Task = (props: props) => {
+export const Task = (props: TaskType & { onChange }) => {
     return (
         <li className="m-4 p-4 flex justify-between items-end bg-teal-200 shadow rounded">
-            <figure className="m-0 p-0">
+            <figure className="m-0 p-0 w-10/12">
                 <blockquote className="text-left text-2xl text-gray-50">
                     <p>{props.title}</p>
                 </blockquote>
                 <figcaption className="italic">{props.title}</figcaption>
             </figure>
-            <Link
-                to={`/todos/${props.id}`}
-                className="bg-teal-700  hover:bg-teal-800 text-white font-bold py-2 px-4 border-b-4 border-teal-800 hover:border-teal-300 rounded"
-            >
-                詳細
-            </Link>
+
+            <input
+                id={`check-box${props.id}`}
+                className="check-box"
+                type="checkbox"
+                checked={props.isDone}
+            />
+            <label htmlFor={`check-box${props.id}`}></label>
         </li>
     );
 };
