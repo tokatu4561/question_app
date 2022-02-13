63832821c8835696f99975d1cbfa42143c1b0b0f
tokatu4561 <ryobsk4561@gmail.com>
2022-02-12 22:16:17 -0800

    already auth dont show loginForm

--- backend/resources/js/App.tsx
+++ backend/resources/js/App.tsx
@@ -6,46 +6,50 @@ import {
     Link,
     Redirect,
 } from "react-router-dom";
+import { getUser } from "./api/auth-api";
 
 import { Layout } from "./components/Layout/Layout";
+import { LoadingSpinner } from "./components/UI/LoadingSpinner";
 import { LoginPage } from "./pages/Auth/LoginPage";
 import { NotFound } from "./pages/NotFound";
 import { AllTasks } from "./pages/tasks/AllTasks";
 import { AuthContext } from "./store/auth-context";
 
-// import { Cart } from "./components/Cart/Cart";
-// import { Header } from "./components/Layout/Header";
-// import { Layout } from "./components/Layout/Layout";
-// import { SideBar } from "./components/Layout/SideBar";
-// import { Meals } from "./components/Meals/Meals";
-// import { AllTodos } from "./Pages/AllTodos";
-// import { NewTodo } from "./Pages/NewTodo";
-// import { NotFound } from "./Pages/NotFound";
-// import { TodoDetail } from "./Pages/TodoDetail";
-// import { CartProvider } from "./store/CartProvider";
-
 export const App = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const ctx = useContext(AuthContext);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const login = async () => {
+            const user = await getUser();
+            if (user) {
+                ctx.onLogin();
+            }
+            setIsLoading(false);
+        };
+
+        login();
+    }, []);
 
     return (
         <Layout>
-            <Switch>
-                <Route path="/login">
-                    {ctx.isLoggedIn && <Redirect to="/tasks" />}
-                    <LoginPage />
-                </Route>
-                {!ctx.isLoggedIn && <Redirect to="/login" />}
-                <Route path="/tasks">
-                    <AllTasks />
-                </Route>
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
+            {isLoading && <LoadingSpinner />}
+            {!isLoading && (
+                <Switch>
+                    <Route path="/login">
+                        {ctx.isLoggedIn && <Redirect to="/tasks" />}
+                        <LoginPage />
+                    </Route>
+                    {!ctx.isLoggedIn && <Redirect to="/login" />}
+                    <Route path="/tasks">
+                        <AllTasks />
+                    </Route>
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+            )}
         </Layout>
     );
 };
-
-function About() {
-    return <h2>About</h2>;
-}
