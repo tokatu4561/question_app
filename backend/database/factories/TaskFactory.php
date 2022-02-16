52360e824e9c5db2ae16ef4839afdacb3898345e
tokatu4561 <ryobsk4561@gmail.com>
2022-02-16 06:47:37 -0800

    dummy Data

--- backend/database/factories/TaskFactory.php
+++ backend/database/factories/TaskFactory.php
@@ -16,7 +16,7 @@ class TaskFactory extends Factory
         return [
             'title' => $this->faker->realText(rand(15, 20)),
             'is_done' => $this->faker->boolean(10),
-            'task_theme_id' => 1,
+            'task_theme_id' => rand(1, 3),
             'created_at' => now(),
             'updated_at' => now(),
         ];
--- backend/database/seeders/TaskSeeder.php
+++ backend/database/seeders/TaskSeeder.php
@@ -14,6 +14,6 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        Task::factory(10)->create();
+        Task::factory(20)->create();
     }
 }
