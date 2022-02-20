820c364926e8d4abfba4b0d6de6b69225b6b1525
tokatu4561 <ryobsk4561@gmail.com>
2022-02-19 22:46:37 -0800

    remove feat task sorting

--- backend/resources/js/components/Tasks/TaskList.tsx
+++ backend/resources/js/components/Tasks/TaskList.tsx
@@ -7,16 +7,6 @@ import { NewTaskForm } from "./NewTaskForm";
 import { Task } from "./Task";
 import { v4 as uuid } from "uuid";
 
-const sortTodos = (tasks, ascending) => {
-    return tasks.sort((taskA, taskB) => {
-        if (ascending) {
-            return taskA.id > taskB.id ? 1 : -1;
-        } else {
-            return taskA.id < taskB.id ? 1 : -1;
-        }
-    });
-};
-
 type props = {
     tasks: [];
     themeId: string;
@@ -31,26 +21,6 @@ export const TaskList = (props) => {
     const { sendRequest: addRequest, status, error } = useHttp(addTask, false);
     //タスク削除のAPI(ソフトデリート)
     const { sendRequest: deleteRequest } = useHttp(softDeleteTask, false);
-
-    const history = useHistory();
-    const location = useLocation();
-
-    const queryPrams = new URLSearchParams(location.search);
-    //タスクの昇順、降順切り替え
-    const isSortingAsc = queryPrams.get("sort") === "asc";
-
-    useEffect(() => {
-        setTasks((currentTasks) => {
-            return sortTodos(currentTasks, isSortingAsc);
-        });
-    }, [isSortingAsc]);
-
-    const changeSortingHandler = useCallback(() => {
-        history.push({
-            pathname: location.pathname,
-            search: `?sort=${isSortingAsc ? "desc" : "asc"}`,
-        });
-    }, [location, isSortingAsc]);
 
     //新規タスク追加フォームの切り替え
     const startAddTaskHandler = () => {
@@ -88,15 +58,7 @@ export const TaskList = (props) => {
     };
 
     return (
-        <Fragment>
-            <div className="pb-4 mb-8 border-b-4 border-inherit">
-                <button
-                    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                    onClick={changeSortingHandler}
-                >
-                    {isSortingAsc ? "降順" : "昇順"}に並び替え
-                </button>
-            </div>
+        <>
             <ul className="list-none m-0 p-0">
                 {tasks.length > 0 &&
                     tasks.map((task) => (
@@ -124,6 +86,6 @@ export const TaskList = (props) => {
                     <button onClick={endAddTaskHandler}>閉じる</button>
                 </>
             )}
-        </Fragment>
+        </>
     );
 };
