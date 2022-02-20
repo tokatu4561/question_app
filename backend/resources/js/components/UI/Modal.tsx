d37b1061f2aeb15853de3d199af83f150eca39b9
tokatu4561 <ryobsk4561@gmail.com>
2022-02-19 21:53:47 -0800

    add modal component

---
+++ backend/resources/js/components/UI/Modal.tsx
@@ -0,0 +1,36 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+    return (
+        <div
+            className="fixed top-0 left-0 w-full h-screen z-20 opacity-50 bg-black"
+            onClick={props.onClose}
+        />
+    );
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className="modal">
+            <div className="">{props.children}</div>
+        </div>
+    );
+};
+
+const portalElement = document.getElementById("modal-overlays");
+
+export const Modal = (props) => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClose={props.onClose} />,
+                portalElement
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay>{props.children}</ModalOverlay>,
+                portalElement
+            )}
+        </Fragment>
+    );
+};
