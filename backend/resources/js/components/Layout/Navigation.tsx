7cab49b7cf48ce8aa38ee2d677940b7cfca28975
tokatu4561 <ryobsk4561@gmail.com>
2022-02-20 04:45:35 -0800

    change style navigation menu

--- backend/resources/js/components/Layout/Navigation.tsx
+++ backend/resources/js/components/Layout/Navigation.tsx
@@ -9,32 +9,18 @@ export const Navigation = (props: props) => {
     const { authUser } = useAuthUser();
 
     return (
-        <header className="flex p-8 justify-between h-12 items-center bg-stone-500">
+        <header className="flex p-8 justify-between h-12 items-center bg-stone-700">
             <div className="">
                 <button
                     onClick={props.onClickShow}
                     className="relative h-5 w-6"
                 >
-                    <span className="top-0 w-8 h-1 inline-block transition-all absolute right-0 bg-black"></span>
-                    <span className="top-3 w-8 h-1 inline-block transition-all absolute right-0 bg-black"></span>
-                    <span className="top-6 w-8 h-1 inline-block transition-all absolute right-0 bg-black"></span>
+                    <span className="top-0 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
+                    <span className="top-3 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
+                    <span className="top-6 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
                 </button>
             </div>
-            <nav>
-                <ul className="flex p-0 m-0 list-none">
-                    {authUser && (
-                        <li className="ml-4 text-2xl">
-                            <NavLink
-                                className="text-stone-200"
-                                to="/tasks"
-                                activeClassName="text-stone-100"
-                            >
-                                　Todo一覧
-                            </NavLink>
-                        </li>
-                    )}
-                </ul>
-            </nav>
+            <div className="text-black">タスク管理</div>
         </header>
     );
 };
