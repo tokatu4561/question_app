a21e0d689209078fab9e27078173babdd3606a63
tokatu4561 <ryobsk4561@gmail.com>
2022-02-24 15:25:09 -0800

    change content NotTasks component English to Japanese

--- backend/resources/js/components/Tasks/NoTasksFound.tsx
+++ backend/resources/js/components/Tasks/NoTasksFound.tsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
-
 export const NoTasksFound = () => {
     return (
         <div className="flex justify-center items-center flex-column m-auto h-80">
-            <p className="font-bold text-3xl">No quotes found!</p>
-            <Link to="new-todo" className="btn">
-                Add a Quote
-            </Link>
+            <p className="font-bold text-3xl">タスクがありません</p>
         </div>
     );
 };
