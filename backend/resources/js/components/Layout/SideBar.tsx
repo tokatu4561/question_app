cfd5fa6470154ea5069274c22b0da5b5905467f2
tokatu4561 <ryobsk4561@gmail.com>
2022-02-26 20:04:20 -0800

    change style sideBar

--- backend/resources/js/components/Layout/SideBar.tsx
+++ backend/resources/js/components/Layout/SideBar.tsx
@@ -33,10 +33,10 @@ const SideBarOverlay = (props) => {
                             <NavLink
                                 className="hover:text-gray-800 hover:bg-gray-100 flex items-center p-2 my-6 transition-colors dark:hover:text-white dark:hover:bg-gray-600 duration-200  text-gray-600 dark:text-gray-400 rounded-lg"
                                 to="/trash"
-                                activeClassName="bg-gray-100 dark:bg-gray-600 text-blue-500"
+                                activeClassName="bg-gray-100 dark:bg-gray-600"
                             >
                                 <i className="fas fa-trash-alt"></i>
-                                <span className="mx-4 text-md font-normal">
+                                <span className="mx-4 text-md font-normal text-blue-500">
                                     ゴミ箱
                                 </span>
                             </NavLink>
