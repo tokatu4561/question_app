99d3237be38251e722a8bc3a771fd72c73dc5c11
tokatu4561 <ryobsk4561@gmail.com>
2022-02-26 20:10:50 -0800

    change style header

--- backend/resources/js/components/Layout/Navigation.tsx
+++ backend/resources/js/components/Layout/Navigation.tsx
@@ -9,17 +9,12 @@ export const Navigation = (props: props) => {
     const { authUser } = useAuthUser();
 
     return (
-        <header className="flex p-8 justify-between h-12 items-center bg-stone-700">
-            <div className="">
-                <button
-                    onClick={props.onClickShow}
-                    className="relative h-5 w-6"
-                >
-                    <span className="top-0 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
-                    <span className="top-3 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
-                    <span className="top-6 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
-                </button>
-            </div>
+        <header className="flex p-8 justify-between h-12 items-center bg-stone-500">
+            <button onClick={props.onClickShow} className="relative h-5 w-6">
+                <span className="top-0 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
+                <span className="top-3 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
+                <span className="top-6 w-8 h-1 inline-block transition-all rounded-full absolute right-0 bg-black"></span>
+            </button>
             <div className="text-black">タスク管理</div>
         </header>
     );
