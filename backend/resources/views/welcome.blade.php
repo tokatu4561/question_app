f82f5f1a517d96686210cea6041025a9359e09ab
tokatu4561 <ryobsk4561@gmail.com>
2022-02-26 21:13:20 -0800

    cdn fontawsome

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
@@ -18,6 +19,7 @@
 <body>
     <div id="root"></div>
     <div id="overlays"></div>
+    <div id="modal-overlays"></div>
 </body>
 
 </html>
