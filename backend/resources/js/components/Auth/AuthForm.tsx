e178f1078128e1904656b39bb3165a7b231db1fc
tokatu4561 <ryobsk4561@gmail.com>
2022-02-26 17:59:45 -0800

    change style login form

--- backend/resources/js/components/Auth/AuthForm.tsx
+++ backend/resources/js/components/Auth/AuthForm.tsx
@@ -29,55 +29,60 @@ export const AuthForm = () => {
     };
 
     return (
-        <Card>
-            <form
-                className="text-center text-stone-800"
-                onSubmit={submitLoginForm}
-            >
-                <div className="mb-2">
-                    <label className="mb-2 block font-bold" htmlFor="email">
-                        ログインID
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={enteredEmail}
-                        onChange={changeEmailHandler}
-                        className="p-2 border border-stone-700 rounded"
-                        required
-                    />
-                </div>
-                <div className="mb-2">
-                    <label className="mb-2 block font-bold" htmlFor="password">
-                        パスワード
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="p-2 border border-stone-700 rounded"
-                        value={enteredPassword}
-                        onChange={changePasswordHandler}
-                        required
-                    />
-                </div>
-                <div className="mb-2">
-                    <p className="text-xs">
-                        [テストユーザー]
-                        <br />
-                        ログインID: test@test.com
-                        <br />
-                        パスワード: test1234
-                    </p>
-                </div>
-                <div className="mt-3 flex flex-col items-center">
-                    <button
-                        type="submit"
-                        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                    >
-                        ログイン
-                    </button>
-                </div>
-            </form>
-        </Card>
+        <div className="max-w-2xl mx-auto mt-16">
+            <Card>
+                <form
+                    className="text-center text-stone-800"
+                    onSubmit={submitLoginForm}
+                >
+                    <div className="mb-2">
+                        <label className="mb-2 block font-bold" htmlFor="email">
+                            ログインID
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={enteredEmail}
+                            onChange={changeEmailHandler}
+                            className="p-2 border border-stone-700 rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            className="mb-2 block font-bold"
+                            htmlFor="password"
+                        >
+                            パスワード
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="p-2 border border-stone-700 rounded"
+                            value={enteredPassword}
+                            onChange={changePasswordHandler}
+                            required
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <p className="text-xs">
+                            [テストユーザー]
+                            <br />
+                            ログインID: test@test.com
+                            <br />
+                            パスワード: test1234
+                        </p>
+                    </div>
+                    <div className="mt-3 flex flex-col items-center">
+                        <button
+                            type="submit"
+                            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                        >
+                            ログイン
+                        </button>
+                    </div>
+                </form>
+            </Card>
+        </div>
     );
 };
