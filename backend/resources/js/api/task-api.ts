2c5dee4c5982f915f28d8780cc2bed9cd651f46c
tokatu4561 <ryobsk4561@gmail.com>
2022-02-27 15:42:15 -0800

    fix task api erro handler

--- backend/resources/js/api/task-api.ts
+++ backend/resources/js/api/task-api.ts
@@ -2,68 +2,75 @@ import axios from "../../../node_modules/axios/index";
 
 // 指定したテーマ内の全てのタスクを取得
 export async function getAllTasks(taskThemeId) {
-    const response = await axios.get(`/api/tasks?theme=${taskThemeId}`);
-    const data = await response.data;
+    try {
+        const response = await axios.get(`api/tasks?theme=${taskThemeId}`);
+        const data = await response.data;
 
-    // if (!response.ok) {
-    //     throw new Error(data.message || "タスクがありません");
-    // }
-
-    return data;
+        return data;
+    } catch (error) {
+        throw new Error(error || "");
+    }
 }
 
 //タスクを追加する
 export async function addTask(taskData) {
-    const response = await axios.post("/api/tasks", {
-        id: taskData.taskId,
-        title: taskData.title,
-        themeId: taskData.themeId,
-    });
-    const data = await response.data;
+    try {
+        const response = await axios.post("api/tasks", {
+            id: taskData.taskId,
+            title: taskData.title,
+            themeId: taskData.themeId,
+        });
+        const data = await response.data;
 
-    // if (!response.ok) {
-    //     throw new Error(data.message || "Could not create quote.");
-    // }
-
-    return null;
+        return null;
+    } catch (error) {
+        throw new Error(error || "");
+    }
 }
 
 //タスクを削除する(ソフトデリート)
 export async function softDeleteTask(taskId: string) {
-    const response = await axios.delete(`/api/tasks/${taskId}`);
-    const data = await response.data;
-
-    // if (!response.ok) {
-    //     throw new Error(data.message || "Could not create quote.");
-    // }
-
-    return null;
+    try {
+        const response = await axios.delete(`api/tasks/${taskId}`);
+        const data = await response.data;
+        return null;
+    } catch (error) {
+        throw new Error(error || "");
+    }
 }
 
 //　削除されているタスク(ソフトデリートされたタスク)を取得する
 export async function getDeletedTasks() {
-    const response = await axios.get(`/api/trash`);
-    const data = await response.data;
+    try {
+        const response = await axios.get(`api/trash`);
+        const data = await response.data;
 
-    // if (!response.ok) {
-    //     throw new Error(data.message || "タスクがありません");
-    // }
-
-    return data;
+        return data;
+    } catch (error) {
+        throw new Error(error || "");
+    }
 }
 
 //タスクをゴミ箱から削除する(フォースデリート)
 export async function forceDeleteTask() {
-    const response = await axios.delete("/api/trash");
-    const data = await response.data;
+    try {
+        const response = await axios.delete("api/trash");
+        const data = await response.data;
 
-    return null;
+        return null;
+    } catch (error) {
+        throw new Error(error || "");
+    }
 }
 
 //タスクを復元する
 export async function restoreTask(id: string) {
-    const response = await axios.post(`/api/tasks-restore/${id}`);
-    const data = await response.data;
+    try {
+        const response = await axios.post(`api/tasks-restore/${id}`);
+        const data = await response.data;
 
-    return data;
+        return data;
+    } catch (error) {
+        throw new Error(error || "タスクを復元できませんでした");
+    }
 }
