4165beb0c9f254c80bb1eacdaf39e5c425b6497d
tokatu4561 <ryobsk4561@gmail.com>
2022-03-04 22:41:03 -0800

    change login form use input custum hooks

--- backend/resources/js/components/Auth/AuthForm.tsx
+++ backend/resources/js/components/Auth/AuthForm.tsx
@@ -2,28 +2,47 @@ import { ChangeEvent, useState } from "react";
 import axios from "../../../../node_modules/axios/index";
 
 import { useAuthUser } from "../../hooks/use-auth-user";
+import { useInput } from "../../hooks/use-input";
 import { User } from "../../types/user";
 import { Card } from "../UI/Card";
 import { LoadingSpinner } from "../UI/LoadingSpinner";
 
+const isFiveOrMore = (value: string) => value.trim().length >= 5;
+
 export const AuthForm = () => {
     const { onLogin } = useAuthUser();
 
-    const [enteredEmail, setEnterdEmail] = useState<string>("");
-    const [enteredPassword, setEnterdPassword] = useState<string>("");
+    const {
+        value: enteredEmail,
+        isValid: emailIsValid,
+        hasError: emailHasError,
+        valueChangeHandler: changeEmailHandler,
+        inputBlurHandler: blurEmailHandler,
+        reset: resetEmail,
+    } = useInput(isFiveOrMore);
+
+    const {
+        value: enterdPassword,
+        isValid: passwordIsValid,
+        hasError: passwordHasError,
+        valueChangeHandler: changePasswordHandler,
+        inputBlurHandler: blurPasswordHandler,
+        reset: resetPassword,
+    } = useInput(isFiveOrMore);
+
+    let formIsValid = false;
+
+    if (emailIsValid && passwordIsValid) {
+        formIsValid = true;
+    }
+
+    //ログインフォーム送信時のエラーとローディング状態
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
 
-    const changeEmailHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setEnterdEmail(e.target.value);
-    };
-    const changePasswordHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setEnterdPassword(e.target.value);
-    };
-
     const submitLoginForm = async (e) => {
         e.preventDefault();
-        const authData = { email: enteredEmail, password: enteredPassword };
+        const authData = { email: enteredEmail, password: enterdPassword };
 
         setIsLoading(true);
         try {
@@ -35,6 +54,8 @@ export const AuthForm = () => {
         } catch (error) {
             setIsError(true);
             setIsLoading(false);
+            resetEmail();
+            resetPassword();
         }
     };
 
@@ -65,9 +86,15 @@ export const AuthForm = () => {
                             id="email"
                             value={enteredEmail}
                             onChange={changeEmailHandler}
+                            onBlur={blurEmailHandler}
                             className="p-2 border border-stone-700 rounded"
                             required
                         />
+                        {emailHasError && (
+                            <p className="text-red-500">
+                                　５文字以上のログインIDを入力してください
+                            </p>
+                        )}
                     </div>
                     <div className="mb-2">
                         <label
@@ -80,10 +107,16 @@ export const AuthForm = () => {
                             type="password"
                             id="password"
                             className="p-2 border border-stone-700 rounded"
-                            value={enteredPassword}
+                            value={enterdPassword}
                             onChange={changePasswordHandler}
+                            onBlur={blurPasswordHandler}
                             required
                         />
+                        {passwordHasError && (
+                            <p className="text-red-500">
+                                ５文字以上のパスワードを入力してください
+                            </p>
+                        )}
                     </div>
                     <div className="mb-2">
                         <p className="text-xs">
@@ -97,7 +130,8 @@ export const AuthForm = () => {
                     <div className="mt-3 flex flex-col items-center">
                         <button
                             type="submit"
-                            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow disabled:opacity-25"
+                            disabled={!formIsValid}
                         >
                             ログイン
                         </button>
